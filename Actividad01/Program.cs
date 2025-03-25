7e4a549dd0dad766ecc34315aff9839c0e3a944a
JulianaChoque <juliana.choque0234@gmail.com>
2025-03-25 19:01:43 -0300

    Tarea realizada

--- Actividad01/Program.cs
+++ Actividad01/Program.cs
@@ -9,4 +9,42 @@ namespace Actividad01
             Console.WriteLine("Hello World!");
         }
     }
+
+    public class gym
+    {
+        public int cantmaquinas;
+        public string nombrermaquina;
+        public string musculoquetrabaja;
+
+        public void descanso()
+        {
+            Console.WriteLine("En descanso");
+        }
+
+        public void enactividad()
+        {
+            Console.WriteLine("En actividad");
+        }
+    }   
+
+    public class perros
+    {
+        public string nombre;
+        public string raza;
+        public int edad;
+        public float peso;
+
+        public void comiendo()
+        {
+            Console.WriteLine("El perro esta comiendo");
+        }
+        public void ladrar()
+        {
+            Console.WriteLine("El perro esta ladrando");
+        }
+        public void durmiendo()
+        {
+            Console.WriteLine("El perro esta durmiendo");
+        }
+    }
 }
